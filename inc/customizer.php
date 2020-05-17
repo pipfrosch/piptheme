1d56c6a347913bfd4cf6583a2cb44f38c999347d
Alice Wonder <naughty@deviant.email>
2020-05-16 17:12:57 -0700

    customizer timestamp in filename

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,7 +21,7 @@ add_action( 'customize_register', 'piptheme_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function piptheme_customize_preview_js() {
-	wp_enqueue_script( 'piptheme_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( 'piptheme_customizer', get_template_directory_uri() . '/js/customizer-20130508.js', array( 'customize-preview' ), null, true );
 }
 add_action( 'customize_preview_init', 'piptheme_customize_preview_js' );
 
